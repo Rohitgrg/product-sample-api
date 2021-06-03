76ebfc9976c42727095f94282877bbc32b7cbcac
Rohit Gurung <rohitgrg75@gmail.com>
2021-06-03 23:30:37 +1000

    UPDATE: package.json to support heroku deploy

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # product-sample-api
+
+## Description:
+
+A simple application written in typescript which provides create,read, update and delete api for a simple product with id, name, description and price.
+
+```js
+{
+    'id' : 123,
+    'name' : 'Sample Product',
+    'description' : 'Sample Product Description',
+    'price': 123
+}
+```
+
+## Installations and Setup Instructions
+
+### Pre-requisites
+
+- **node** and **npm** installed globally in your machine
+
+You can download node from :
+`https://nodejs.org/en/download/current/`
+
+### Installation guide:
+
+1. `npm i`
+2. `npm start`
+3. Visit application: `localhost:5000`
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { DatabaseUtil, IDbConfig } from "./db/db";
 import { ApplicationRouter } from "./router/router";
 import { authenticate } from "./middleware/auth";
 import { errorHandler } from "./plugins/errorHandler";
-
+import md from "markdown-it";
+import fs from "fs";
+import path from "path";
 class Server {
   public static async startServer() {
     const application: Express = express();
@@ -21,6 +23,15 @@ class Server {
     await database.startMongoDB();
 
     application.use(express.json());
+    application.get("/", (req, res) => {
+      const markdownRender = md();
+      const filePath = path.join(__dirname, "../README.md");
+      const data = fs.readFileSync(filePath, "utf8");
+      const result = markdownRender.render(data);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(result);
+      res.end();
+    });
     application.use(authenticate);
     application.use("/api", new ApplicationRouter().getRouter());
     application.use(errorHandler);
